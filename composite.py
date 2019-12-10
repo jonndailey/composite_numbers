02964f51164fa80faac1a23c4ae788165a4b29f7
Jonny Dailey <jonndailey@gmail.com>
2019-12-10 15:01:50 -0500

    Initial commit.

---
+++ composite.py
@@ -0,0 +1,23 @@
+# Prime Numbers
+# Aeriel Denmark
+# Thurs 12/5/19
+
+# www.practicepython.org
+
+# Write a program that determines if the number entered by the user
+# is a prime number or not
+
+#need to print the composite numbers
+
+import math
+
+num = int(raw_input("Enter a number: "))
+
+if num > 1:
+	for i in range (2, num):
+		if (num % i) == 0:
+			print num, "is not a prime number"
+			break
+		else:
+			print num, "is a prime number"
+			break
